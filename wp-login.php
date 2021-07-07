4206f901eb43c277e6dc953c90f0e3be8a9b1351
namlem4u <namlem4u@gmail.com>
2021-07-07 21:38:41 +0700

    fix UI last test

--- wp-login.php
+++ wp-login.php
@@ -85,7 +85,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 </script>
 <script src="/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script id="user-profile-js-extra">
-var userProfileL10n = {"user_id":"0","nonce":"835647a1e3"};
+var userProfileL10n = {"user_id":"0","nonce":"db8f4b36d4"};
 </script>
 <script id="user-profile-js-translations">
 ( function( domain, translations ) {
